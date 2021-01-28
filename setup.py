8b546bc507d77d7db7ae8f76a961e09575c80c0c
Monster <monster@localhost.localdomain>
2021-01-27 20:51:41 -0700

    THIS IS NOT TESTED Added the setup.py logic with reading README.md and requirements.txt to work Added the bin dir to hold all scripts Added blank script pdfCrab in bin dir TODO add logic to the bin/pdfCrab and add venv for dev

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/bin/env python3
+"""
+This is the setup script for the pdfCrab
+Reads the README.md for the long description for the application
+Reads the requirements.txt to install all proper modules for the app
+TO INSTALL:
+    FULL INSTALL TO PATH: python3 setup.py install
+    DEV INSTALL ADD SYM LINK: python3 setup.py develop
+"""
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+with open("requirements.txt", 'r') as d:
+    requirements = d.read().splitlines()
+
+setup(
+    name='pdfCrab',
+    version='0.1',
+    author='TrueSonOfaGun',
+    author_email='idonthaveone@fake.email',
+    description='Used to grab pdf file from a domain',
+    python_version='>=3.0',
+    long_description=long_description,
+    url="https://github.com/truesonofagun/pdfCrab",
+    packages=['pdfCrab'],
+    install_requires=requirements,
+    scripts=['bin/pdfCrab']
+)
